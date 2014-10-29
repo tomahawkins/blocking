e158cd82f5098ec5c430dce47cbe2cb3a68efd08
Tom Hawkins <tomahawkins@gmail.com>
2014-10-29 09:22:02 -0400

    Updates.

--- Blocking.hs
+++ Blocking.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import Data.List
 import System.Environment
 import Text.Printf
 
@@ -20,49 +21,44 @@ data Result
 
 type Attack = (Block, Result)
 
+type Condition = Attack -> Bool
+
 main :: IO ()
 main = do
   args <- getArgs
-  attacks <- mapM readFile args >>= return . map parseAttack . lines . concat
-  let percent msg a b = printf (msg ++ " : %2.0f%%\n") $ percentage attacks a b
-  printf  "Total recorded attacks                        : %d\n"      $ length attacks
-  percent "Block attempts   of total attacks            " blockAttempt  allAttacks
-  percent "Block touches    of block attempts           " blockTouched  blockAttempt
-  percent "Block misses     of block attempts           " blockMissed   blockAttempt
+  attacks <- mapM readFile args >>= return . map (map parseAttack . lines)
+  let report' = report attacks
+  report' "Blocks touched   of all blocks    " blockTouched  blockAttempt
   putStrLn ""
-  percent "Win point        of block attempts           " winPoint      blockAttempt
-  percent "Lost point       of block attempts           " lostPoint     blockAttempt
+  report' "Win point        of all blocks    " winPoint      blockAttempt
+  report' "Lost point       of all blocks    " lostPoint     blockAttempt
   putStrLn ""
-  percent "Win point        of block touched            " winPoint      blockTouched
-  percent "Lost point       of block touched            " lostPoint     blockTouched
+  report' "Win point        of blocks touched" winPoint      blockTouched
+  report' "Lost point       of blocks touched" lostPoint     blockTouched
   putStrLn ""
-  percent "Win point        of block missed             " winPoint      blockMissed
-  percent "Lost point       of block missed             " lostPoint     blockMissed
+  report' "Win point        of blocks missed " winPoint      blockMissed
+  report' "Lost point       of blocks missed " lostPoint     blockMissed
   putStrLn ""
-  percent "Win point        of block missed or no block " winPoint      noBlockOrMissed
-  percent "Lost point       of block missed or no block " lostPoint     noBlockOrMissed
-  putStrLn ""
-  percent "Win point        of no block                 " winPoint      noBlock
-  percent "Lost point       of no block                 " lostPoint     noBlock
-  putStrLn ""
-  percent "Gained advantage of block attempts           " gainAdvantage blockAttempt
-  percent "Gained advantage of block touched            " gainAdvantage blockTouched
-  percent "Gained advantage of block missed             " gainAdvantage blockMissed
-  percent "Gained advantage of block missed or no block " gainAdvantage noBlockOrMissed
-  percent "Gained advantage of no block                 " gainAdvantage noBlock
+  report' "Gained advantage of all blocks    " gainAdvantage blockAttempt
+  report' "Gained advantage of blocks touched" gainAdvantage blockTouched
+  report' "Gained advantage of blocks missed " gainAdvantage blockMissed
 
-allAttacks             = const True
+report :: [[Attack]] -> String -> Condition -> Condition -> IO ()
+report attacks msg a b = putStrLn $ msg ++ " : " ++ intercalate "  " [ printf "%2.0f%%" $ percentage attacks a b | attacks <- attacks ]
+
+-- Conditions.
 winPoint        (_, r) = case r of { WinPoint  -> True; _ -> False }
 lostPoint       (_, r) = case r of { LostPoint -> True; _ -> False }
 gainAdvantage   (_, r) = case r of { WinPoint -> True; DefenseAttack -> True; OffenseFreeBall -> True; _ -> False }
---lostAdvantage          = not . gainAdvantage
 blockTouched    (b, _) = case b of { BlockTouch -> True; _ -> False }
-noBlockOrMissed        = not . blockTouched
 blockMissed     (b, _) = case b of { BlockMiss  -> True; _ -> False }
 blockAttempt    (b, _) = case b of { BlockTouch -> True; BlockMiss -> True; NoBlock -> False }
-noBlock                = not . blockAttempt
+--allAttacks             = const True
+--lostAdvantage          = not . gainAdvantage
+--noBlockOrMissed        = not . blockTouched
+--noBlock                = not . blockAttempt
 
-percentage :: [Attack] -> (Attack -> Bool) -> (Attack -> Bool) -> Double
+percentage :: [Attack] -> Condition -> Condition -> Double
 percentage attacks a b = 100 * fromIntegral (length a') / fromIntegral (length b')
   where
   b' = filter b attacks
